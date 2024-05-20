35b8ac8f4b43cfeecfc2ddebe14718657c2b7f38
Donghee Kim <hongricky73@naver.com>
2024-05-20 18:35:19 +0900

    detail.dart update

--- lib/detail.dart
+++ lib/detail.dart
@@ -25,10 +25,49 @@ class DetailScreen extends StatelessWidget {
                   color: Colors.white,
                 ),
                 padding: EdgeInsets.all(16.0),
-                child: Text(
-                  // 여기에 글 내용을 적어주세요. 길어질 경우 스크롤 가능합니다.
-                  '이것은 플러터로 만든 글 내용입니다. 글이 매우 길어 스크롤해서 읽을 수 있습니다.',
-                  style: TextStyle(fontSize: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Posted in Be전공자 지식나눔', //이 부분에 데이터 받아와야함
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    Divider(),
+                    SizedBox(height: 8.0),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.blue,
+                          child: Icon(Icons.person, color: Colors.white),
+                        ),
+                        SizedBox(width: 8.0,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('김태민'),
+                            Text('수원대학교, 컴퓨터학부'
+                            ),
+                            SizedBox(height: 8.0,)
+                          ],
+                        )
+                      ],
+                    ),
+                    Text(
+                      "Finally 블록에서 예외가 발생하면 어떻게 되나요?",   //글 제목 넣기
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),
+                    ),
+                    Text(
+                    "자바에서, try-catch에서 finally 블록 내에서 예외가 발생할 때의 동작은, try-catch 블록의 예외 처리와 약간 "
+                        "다릅니다.  [Try-Catch 블록의 예외]정상적인 플로우가 중지됩니다"
+                        ".예외 유형과 일치하는 catch 블록이 있다면, 해당 블록 내의 코드가 실행됩니다. 이를"
+                        " 통해 예외를 처리할 수 있습니다.catch 블록이 완료되거나 어떤 catch 블"
+                        "록과도 일치하지 않으면 finally 블록의 코드가 실행됩니다.[Finally 블록의 예외]원래의 예외가 중지됩니다. 이는 원래의 예"
+                        "외가 메서드 호출자에게 전달되지 않는 것을 의미합니다.finally 블록에서 던진 예외가 호출자에게전달되지 않는 것을 의미합니다.finally 블록에서 던진 "
+                        "예외가 호출자에게 전파되는 새로운 예외가 됩니다.", //글 내용!
+                      style: TextStyle(fontSize: 18.0),
+                        )
+                  ],
                 ),
               ),
               SizedBox(height: 20.0), // 공간 추가
@@ -48,11 +87,10 @@ class DetailScreen extends StatelessWidget {
                 ),
                 height: 300.0, // 댓글이 많을 경우 스크롤 가능하도록 높이 지정
                 child: ListView(
-                  // 댓글 목록 추가
                   children: [
                     ListTile(
-                      title: Text('사용자1'),
-                      subtitle: Text('첫 번째 댓글입니다.'),
+                      title: Text('사용자1'), //memberid 넣으면댐
+                      subtitle: Text('첫 번째 댓글입니다.'),  //comment 넣으면댐
                     ),
                     ListTile(
                       title: Text('사용자2'),
